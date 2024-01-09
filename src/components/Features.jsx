990789f2e3cf3d764216f37026c3ecdd834edfa7
nash300 <nadeesha300@yahoo.co.uk>
2024-01-09 15:58:38 +0100

    inserted dummy text into navigation pages

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,42 @@
 const Features = () => {
-  return <span>Features</span>;
+  const style = {
+    padding: "50px",
+    height: "500px",
+  };
+  return (
+    <span id="fe" style={style}>
+      {" "}
+      <h2>Current Version</h2>
+      andae non delectus sectetur adipisicing elit. Aspernatur labore sequi
+      dolor quos est, autem vero fugiat eaque sed eos illum, dolorem id dolor
+      sit, amet consectetur adipisicing elit. Cupiditate laboriosam assumenda
+      consectetur ad, itaque blanditiis, ut consequatur earum eaque quam quis,
+      obcaecati officiis animi corrupti impedit! Quis iure voluptatum ducimus?
+      <ul>
+        <li>adipisicing</li>
+        <li>animi</li>
+        <li>impedit</li>
+        <li>illum</li>
+        <li>Aspernatur</li>
+      </ul>
+      <h2>Upcomming Version</h2> Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Optio hic velit aliquam eos? Molestiae officia earum
+      odit nesciunt veniam, pariatur repellat eaque cum. Recusandae iste
+      consequatur obcaecati molestias delectus eos!
+      <ul>
+        <li>adipisicing</li>
+        <li>animi</li>
+        <li>impedit</li>
+        <li>adipisicing</li>
+        <li>animi</li>
+        <li>impedit</li>
+        <li>illum</li>
+        <li>Aspernatur</li>
+        <li>illum</li>
+        <li>Aspernatur</li>
+      </ul>
+    </span>
+  );
 };
 
 export default Features;
